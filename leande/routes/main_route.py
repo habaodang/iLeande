c4a42a41d61b9289be508db985e92585d6f58ac5
Rin Bảo <habaodang.hbd@gmail.com>
2025-04-17 13:57:48 +0700

    update video call

--- leande/routes/main_route.py
+++ leande/routes/main_route.py
@@ -5,3 +5,7 @@ main = blueprints.Blueprint('main', __name__)
 @main.route('/')
 def index():
     return render_template('index.html')
+
+@main.route('/videocall')
+def videocall():
+    return  render_template('videocall.html')
